7e34f1ea95831687b1081073757bbdc2e828b37a
Sayardiss <sayardiss@gmail.com>
2018-02-27 22:32:53 +0100

    Ajout de CollectiveHTML

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,60 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+// // Aller chercher le controller dans WelcomeController
+// Route::get('index', 'WelcomeController@index');
+//
+//
+//
+// Route::get('/toto', function () {
+//     return redirect('foo');
+// });
+//
+// Route::get('foo', function () {
+//     return Response::make('Hello error', 500);
+// });
+//
+//
+// // Paramètre à une vue PHP
+// Route::get('article/{n}', function($n) {
+//     return view('article')->with('numero', $n);
+//     //ou return view('article')->withNumero($n);
+//     //ou return view('article', ['numero' => $n]);
+// })->where('n', '[0-9]+');
+//
+// Route::get('articlecontroller/{n}', 'ArticleController@show')->where('n', '[0-9]+');
+//
+//
+// Route::get('{n}', function($n) {
+// 	return 'Je suis la page ' . $n . ' !';
+// })->where('n', '[1-3]');
+//
+// Route::get('facture/{n}', function($n) {
+//     return view('facture')->withNumero($n);
+// })->where('n', '[0-9]+');
+//
+//
+// //// Partie formulaires
+// Route::get('users', 'UsersController@getInfos');
+// Route::post('users', 'UsersController@postInfos');
+//
+// // implicite (RESTful) doesn't work
+// //Route::resource('users', 'UsersController');
+//
+// //// Partie validation
+// Route::get('contact', 'ContactController@getForm');
+// Route::post('contact', 'ContactController@postForm');
+//
+// Route::get('photo', 'PhotoController@getForm');
+// Route::post('photo', 'PhotoController@postForm');
+//
+// //Migrations
+// Route::get('email', 'EmailController@getForm');
+// Route::post('email', ['uses' => 'EmailController@postForm', 'as' => 'storeEmail']);
+//
+//
+// // Ressources 1/2
+// Route::resource('user', 'UserController');
