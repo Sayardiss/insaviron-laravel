d00274b2cf29ac7dcb3b0df87759363490b478a8
sayardiss <sayardiss@gmail.com>
2018-03-01 20:24:04 -0500

    Ajout de la page des news

---
+++ resources/views/news.blade.php
@@ -0,0 +1,169 @@
+@extends('template')
+
+@section('titre')
+    Programmation, résultats et news
+@stop
+
+@section('contenu')
+  <section id="middle" class="fadeInDown">
+      <div class="container">
+
+              <div class="wow fadeInDown">
+                  <div class="accordion">
+                      <h2>Programmation, résultats et news</h2>
+                      <div class="panel-group" id="accordion1">
+
+                        <!-- Premiere ligne -->
+                        <div class="panel panel-default">
+                          <div class="panel-heading active">
+                            <h3 class="panel-title">
+                              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapseOne1">
+                                Event1
+                                <i class="fa fa-angle-right pull-right"></i>
+                              </a>
+                            </h3>
+                          </div>
+
+                          <div id="collapseOne1" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                <div class="media accordion-inner">
+                                      <div class="pull-left">
+                                          <img class="img-responsive" src="images/accordion1.png">
+                                      </div>
+                                      <div class="media-body">
+                                           <h4>Adipisicing elit</h4>
+                                           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+                                      </div>
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <!-- Deuxieme ligne -->
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h3 class="panel-title">
+                              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapseTwo1">
+                                Event2
+                                <i class="fa fa-angle-right pull-right"></i>
+                              </a>
+                            </h3>
+                          </div>
+                          <div id="collapseTwo1" class="panel-collapse collapse">
+                            <div class="panel-body">
+                              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor.
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h3 class="panel-title">
+                              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapseThree1">
+                                Résultat compet1
+                                <i class="fa fa-angle-right pull-right"></i>
+                              </a>
+                            </h3>
+                          </div>
+                          <div id="collapseThree1" class="panel-collapse collapse">
+                            <div class="panel-body">
+                              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor.
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h3 class="panel-title">
+                              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapseFour1">
+                                Prochain evenement
+                                <i class="fa fa-angle-right pull-right"></i>
+                              </a>
+                            </h3>
+                          </div>
+                          <div id="collapseFour1" class="panel-collapse collapse">
+                            <div class="panel-body">
+                              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor.
+                            </div>
+                          </div>
+                        </div>
+                      </div><!--/#accordion1-->
+                  </div>
+              </div>
+
+              <!-- Accordion des news -->
+              <div class="wow fadeInDown">
+                  <div class="accordion">
+                      <h2>NEWS</h2>
+                      <div class="panel-group" id="accordionNews">
+                        <div class="panel panel-default">
+                          <!-- active : surligner par défaut celle là (et l'activer plus bas dans le code) -->
+                          <div class="panel-heading">
+                            <h3 class="panel-title">
+                              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionNews" href="#collapseDEUXOne1">
+                                News1
+                                <i class="fa fa-angle-right pull-right"></i>
+                              </a>
+                            </h3>
+                          </div>
+
+                          <!-- classe in : activé par défaut au chargement -->
+                          <div id="collapseDEUXOne1" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                <div class="media accordion-inner">
+                                      <div class="pull-left">
+                                          <img class="img-responsive" src="images/accordion1.png">
+                                      </div>
+                                      <div class="media-body">
+                                           <h4>Adipisicing elit</h4>
+                                           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+                                      </div>
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h3 class="panel-title">
+                              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionNews" href="#collapseDEUXTwo1">
+                                News2
+                                <i class="fa fa-angle-right pull-right"></i>
+                              </a>
+                            </h3>
+                          </div>
+                          <div id="collapseDEUXTwo1" class="panel-collapse collapse">
+                            <div class="panel-body">
+                              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor.
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h3 class="panel-title">
+                              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionNews" href="#collapseDEUXThree1">
+                                Résultat compet1
+                                <i class="fa fa-angle-right pull-right"></i>
+                              </a>
+                            </h3>
+                          </div>
+                          <div id="collapseDEUXThree1" class="panel-collapse collapse">
+                            <div class="panel-body">
+                              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor.
+                            </div>
+                          </div>
+                        </div>
+
+                      </div><!--/#accordion1-->
+                  </div>
+              </div>
+
+
+      </div><!--/.container-->
+  </section><!--/#middle-->
+
+<!-- garbage02 -->
+
+<!-- garbage01 -->
+@stop
