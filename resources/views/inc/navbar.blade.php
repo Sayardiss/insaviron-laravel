376e48f9ae1eab25100c8d60c019b8debecafeef
sayardiss <sayardiss@gmail.com>
2018-05-28 12:22:43 -0400

    Changed Menu Font & size container

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,95 +1,79 @@
-<header id="header">
-  <nav class="navbar navbar-fixed-top" role="banner">
-    {{-- <nav class="navbar navbar-default navbar-static-top"> --}}
-        <div class="container">
-            <div class="navbar-header">
+<header role="banner">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
 
-                <!-- Branding Image -->
-                {{-- <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a> --}}
-            </div>
+<nav id="navbar-primary" class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-primary-collapse" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="navbar-primary-collapse">
+      <ul class="nav navbar-nav">
+        <li class="{{ isActiveRoute('home') }}"><a href="{{ route('home') }}">@lang('messages.navbar.Main')</a></li>
+        <li class="{{ isActiveRoute('infos') }}"><a href="{{ route('infos') }}">@lang('messages.navbar.Info')</a></li>
+        <li class="{{ isActiveRoute('inscription') }}"><a href="{{ route('inscription') }}">@lang('messages.navbar.Inscription')</a></li>
+        <li><a href="{{ route('home') }}"><img id="logo-navbar-middle" src={{URL::asset('images/RowINSA.png')}} width="200" alt="Logo Thing main logo"></a></li>
+        <li class="{{ isActiveRoute('news.index') }}"><a href="{{ route('news.index') }}">@lang('messages.navbar.News')</a></li>
+        <li class="{{ isActiveRoute('gallery') }}"><a href="{{ route('gallery', []) }}">@lang('messages.navbar.Gallery')</a></li>
+        <li class="{{ isActiveRoute('contact') }}"><a href="{{ route('contact') }}">@lang('messages.navbar.Contact')</a></li>
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse" style="width:90%; margin:0 15%;">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    &nbsp;
-                </ul>
 
-                <ul class="nav navbar-nav" >
-                    <li class="{{ isActiveRoute('home') }}"><a href="{{ route('home') }}">@lang('messages.navbar.Main')</a></li>
-                    <li class="{{ isActiveRoute('infos') }}"><a href="{{ route('infos') }}">@lang('messages.navbar.Info')</a></li>
-                    <li class="{{ isActiveRoute('inscription') }}"><a href="{{ route('inscription') }}">@lang('messages.navbar.Inscription')</a></li>
-                    <a href="{{ route('home') }}" class="pull-left" style="display: inline-block; padding-bottom: 5px;">
-                      <img src={{URL::asset('images/RowINSA.png')}} width="180">
-                    </a>
-                    <li class="{{ isActiveRoute('news.index') }}"><a href="{{ route('news.index') }}">@lang('messages.navbar.News')</a></li>
-                    <li class="{{ isActiveRoute('gallery') }}"><a href="{{ route('gallery', []) }}">@lang('messages.navbar.Gallery')</a></li>
-                    <li class="{{ isActiveRoute('contact') }}"><a href="{{ route('contact') }}">@lang('messages.navbar.Contact')</a></li>
+        {{-- Menu des langues --}}
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <img src="/images/flags/{{ Config::get('flags')[App::getLocale()] }}.png" alt="{{ Config::get('languages')[App::getLocale()] }}"/>
+            {{-- Texte --}}
+            {{-- {{ Config::get('languages')[App::getLocale()] }} --}}
+          </a>
+          <ul class="dropdown-menu">
+            @foreach (Config::get('languages') as $lang => $language)
+              @if ($lang != App::getLocale())
+                <li>
+                  <a href="{{ route('lang.switch', $lang) }}">
+                    <img src="/images/flags/{{ Config::get('flags')[$lang] }}.png" alt="{{$language}}"/>
+                  </a>
+                </li>
+              @endif
+            @endforeach
+          </ul>
+        </li>
 
-                    {{-- Menu des langues --}}
-                    <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="/images/flags/{{ Config::get('flags')[App::getLocale()] }}.png" alt="{{ Config::get('languages')[App::getLocale()] }}"/>
-                        {{-- Texte --}}
-                        {{-- {{ Config::get('languages')[App::getLocale()] }} --}}
-                      </a>
-                      <ul class="dropdown-menu">
-                        @foreach (Config::get('languages') as $lang => $language)
-                          @if ($lang != App::getLocale())
-                            <li>
-                              <a href="{{ route('lang.switch', $lang) }}">
-                                <img src="/images/flags/{{ Config::get('flags')[$lang] }}.png" alt="{{$language}}"/>
-                              </a>
-                            </li>
-                          @endif
-                        @endforeach
-                      </ul>
+
+        @auth
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+                <ul class="dropdown-menu">
+                    <li>
+                      <a href="{{ route('dashboard') }}">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
                     </li>
                 </ul>
-
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Accès zone administration -->
-                    @auth
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu">
-                                <li>
-                                  <a href="{{ route('dashboard') }}">Dashboard</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                    @else
-                      {{-- Pas de proposition de login --}}
-                      {{-- <li><a href="{{ route('login') }}">Login</a></li>
-                      <li><a href="{{ route('register') }}">Register</a></li> --}}
-                    @endauth
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header><!--/header-->
+            </li>
+        @else
+          {{-- Pas de proposition de login --}}
+          {{-- <li><a href="{{ route('login') }}">Login</a></li>
+          <li><a href="{{ route('register') }}">Register</a></li> --}}
+        @endauth
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</header><!-- header role="banner" -->
